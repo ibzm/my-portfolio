b8f6aca3115b2f8746ea9f40282d6594fb415da1
ibzm <ibzm99@outlook.com>
2025-04-07 21:20:51 +0100

    test 3

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '',
+  // basePath: '/my-portfolio',
+  // assetPrefix: '/my-portfolio',
   images: { unoptimized: true },
   
   webpack(config) {
