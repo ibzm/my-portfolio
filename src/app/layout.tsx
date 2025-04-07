eab6f822905162d4110dc81cd4ec86eb794990a8
ibzm <ibzm99@outlook.com>
2025-04-07 18:54:32 +0100

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import { ThemeProvider } from "@/context/ThemeContext";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/styles/global.css";
-//import Header from "@/app/components/Header";
 import Header from "@/app/components/Header";
- import Footer from "@/app/components/Footer";
+import Footer from "@/app/components/Footer";
 import Navbar from "@/app/components/Navbar";
 import React from "react";
 
